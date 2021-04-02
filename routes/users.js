33ef079d4947acea48214d0c6630f305cbd8bdfd
Heliomar Vargas <vargasteban@gmail.com>
2021-04-02 17:15:26 -0600

    espacio

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-
 const { validarCampos } = require('../middlewares/validar-campos');
 const { esRolValido, correoExiste, existeUsuarioID } = require('../helpers/validaciones-db');
 
