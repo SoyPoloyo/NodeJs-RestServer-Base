2f6ad7b1b8e10f6cece59cf47cce1e4e9ddfb80d
Heliomar Vargas <vargasteban@gmail.com>
2021-04-03 17:24:25 -0600

    creacion de tokens a heroku

---
+++ controllers/nombres-comerciales.js
@@ -0,0 +1,78 @@
+const { response } = require('express');
+const bcryptjs = require('bcryptjs');
+
+const NombreComercial = require('../models/nombre-comercial');
+
+const nombreComercialGet = async (req, res = response) => {
+    //req,query**
+    //los query son parametros opcionales que vienen en el url
+    //ejp: localhost:puerto/api/usuarios?desde=5&limite=10
+    //esto nos mostraria del usuario 5 en adelante y solo 10
+
+    let { desde = 0, limite = 5, cual = '' } = req.query;
+
+    isNaN(limite) ? limite = 5 : limite = (Number(limite));
+    isNaN(desde) ? desde = 0 : desde = (Number(desde));
+
+    const activos = { estado: true };
+
+    // usamos Promise.all para ejecutar todas las promesas al mismo tiempo y reducir los tiempos de espera
+    // y una desestructuracion de arreglos ya que nos devuelve un arreglo con las respuestas
+    const [total, nombresComerciales, nombreComercial] = await Promise.all([
+
+        NombreComercial.countDocuments(),
+
+        NombreComercial.find()
+            .skip(desde)
+            .limit(limite), // limita los usuarios mostrados
+
+        NombreComercial.find({ nombre: cual })
+
+
+    ])
+
+
+    // obtener nombres - for fun
+    const nombres = nombresComerciales.map(arr => {
+        return arr.nombre
+    })
+
+    let respuesta = {}
+
+    respuesta = (cual == '') ? respuesta = {
+        Info: `nombres comerciales iran aqui`,
+        "total actual en base de datos": total,
+        nombres: nombres,
+        nombreComercial: nombreComercial
+    } :
+        respuesta = {
+            Nota: `Todo sobre el ${cual}`,
+            Info: nombreComercial
+        }
+
+
+    res.json(respuesta);
+
+
+}
+
+const nombreComercialPost = async (req, res) => {
+
+    const info = req.body;
+
+    const nombreComercial = new NombreComercial(info);
+
+    //Guardamos en la base de datos
+    await nombreComercial.save();
+
+    res.json({
+        msg: 'Nombre comercial creado?',
+        nombreComercial
+    })
+}
+
+
+module.exports = {
+    nombreComercialGet,
+    nombreComercialPost
+}
